388b5e12874860bdcb86e0d053da5a5370407e28
marencoalf <alfredomarenco@boletea.com>
2021-08-28 14:19:12 -0500

    agregamos el centro de venta sombrereria

--- resources/views/landing/event.blade.php
+++ resources/views/landing/event.blade.php
@@ -48,13 +48,15 @@
                         @case('coahuila')
                             <h4 class="text-3xl font-bold text-gray-800 text-center mb-3">Centros de venta:</h4>
                             <div class="flex flex-wrap justify-center">
-                                <img class="flex-auto w-20 object-contain object-center mx-1"
+                                <img class="flex-auto w-20 object-contain object-center mx-2"
                                     src="{{ asset('cdvs/imgCintilloColiseoCentenario.png') }}" alt="">
-                                <img class="flex-auto w-20 object-contain object-center mx-1"
+                                <img class="flex-auto w-20 object-contain object-center mx-2"
                                     src="{{ asset('cdvs/imgCintilloBIPS.png') }}" alt="">
-                                <img class="flex-auto w-24 object-contain object-center mx-1"
+                                <img class="flex-auto w-24 object-contain object-center mx-2"
                                     src="{{ asset('cdvs/imgCintilloCimaco.png') }}" alt="">
-                                <img class="md:flex-auto w-13 md:w-20 object-contain object-center mx-1"
+                                <img class="md:flex-auto w-13 md:w-16 object-contain object-center mx-2"
+                                    src="{{ asset('cdvs/imgeSombrererialafe.jpeg') }}" alt="">
+                                <img class="md:flex-auto w-13 md:w-20 object-contain object-center mx-2"
                                     src="{{ asset('cdvs/imgCintilloLos3garcia.png') }}" alt="">
                             </div>
                         @break
