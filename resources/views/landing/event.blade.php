ea61dc1ef334a2e5af20eed193dc97ad183cc40e
marencoalf <alfredomarenco@boletea.com>
2021-09-16 11:32:17 -0500

    agregamos un script

--- resources/views/landing/event.blade.php
+++ resources/views/landing/event.blade.php
@@ -93,7 +93,7 @@
             </div>
         </div>
     </div>
-    <div class="grid md:grid-cols-3 gap-6 items-center bg-white my-12 mx-4 md:mx-44 py-3 shadow-md">
+    <div class="grid md:grid-cols-3 gap-6 items-center bg-white my-12 mx-4 md:mx-44 py-3 shadow-md lg:hidden">
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5976709800052610"
                 crossorigin="anonymous"></script>
         <!-- Anuncio 1 -->
