5d76e404a6494bad439efa97a6f5cdf679b2ac9c
unknown <alfredomarenco@boletea.com>
2021-11-10 11:35:08 -0600

    Cambios en el horario

--- resources/views/livewire/santos.blade.php
+++ resources/views/livewire/santos.blade.php
@@ -20,7 +20,7 @@
                     <x-jet-input-error class="mr-3 text-6xl py-4 font-bold" for="barcode">
                     </x-jet-input-error>
                     @if ($time)
-                        {{ $time['created_at'] }}
+                        {{ Carbon::parse($time['created_at'])->format('l jS \\of F Y h:i:s A') }}
                     @endif
                 </div>
             </div>
