1e772f8e4a0e673b4020b46ef6ee42c270f7bc3b
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2022-04-04 12:49:10 -0500

    Contador

--- resources/views/landing/event.blade.php
+++ resources/views/landing/event.blade.php
@@ -25,6 +25,21 @@
             </div>
         </div>
         <div class="col-span-2 p-4 mt-1 text-center md:text-left">
+         @if ($event->name == 'elcaminoparasalvarunmatrimonio')
+                <a data-type="countdown" data-id="3174182" class="tickcounter"
+                    style="display: block; width: 100%; position: relative; padding-bottom: 25%" title="Countdown"
+                    href="//www.tickcounter.com/">Countdown</a>
+                <script>
+                    (function(d, s, id) {
+                        var js, pjs = d.getElementsByTagName(s)[0];
+                        if (d.getElementById(id)) return;
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = "//www.tickcounter.com/static/js/loader.js";
+                        pjs.parentNode.insertBefore(js, pjs);
+                    }(document, "script", "tickcounter-sdk"));
+                </script>
+            @endif
             <h3 class="text-xl font-semibold">Detalles del evento</h3>
             <h1 class="text-4xl font-bold text-rojo mt-4 mb-2">
                 {{ $event->title }}
