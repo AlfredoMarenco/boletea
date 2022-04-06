cdb1945830f768d6b5e400e8aae73702a1ac1dfe
unknown <alfredomarenco@boletea.com>
2022-04-06 14:05:23 -0500

    Correccion

--- resources/views/landing/event.blade.php
+++ resources/views/landing/event.blade.php
@@ -20,14 +20,14 @@
         {
             "@context": "https://schema.org",
             "@type": "Event",
-            "name": "{{ $event->name }}",
-            "startDate": "{{ $event->created_at }}",
-            "endDate": "{{ $event->updated_at }}",
+            "name": "The Adventures of Kira and Morrison",
+            "startDate": "2025-07-21T19:00-05:00",
+            "endDate": "2025-07-21T23:00-05:00",
             "eventAttendanceMode": "https://schema.org/OfflineEventAttendanceMode",
             "eventStatus": "https://schema.org/EventScheduled",
             "location": {
                 "@type": "Place",
-                "name": "{{ $event->recinto }}",
+                "name": "Snickerpark Stadium",
                 "address": {
                     "@type": "PostalAddress",
                     "streetAddress": "100 West Snickerpark Dr",
@@ -35,15 +35,30 @@
                     "postalCode": "19019",
                     "addressRegion": "PA",
                     "addressCountry": "US"
-                  }
-            }
+                }
+            },
             "image": [
-                "{{ $event->image }}",
+                "https://example.com/photos/1x1/photo.jpg",
+                "https://example.com/photos/4x3/photo.jpg",
+                "https://example.com/photos/16x9/photo.jpg"
             ],
-            "description": "{{ $event->description }}",
+            "description": "The Adventures of Kira and Morrison is coming to Snickertown in a can't miss performance.",
+            "offers": {
+                "@type": "Offer",
+                "url": "https://www.example.com/event_offer/12345_201803180430",
+                "price": "30",
+                "priceCurrency": "USD",
+                "availability": "https://schema.org/InStock",
+                "validFrom": "2024-05-21T12:00"
+            },
             "performer": {
                 "@type": "PerformingGroup",
-                "name": "{{ $event->subtitle }}",
+                "name": "Kira and Morrison"
+            },
+            "organizer": {
+                "@type": "Organization",
+                "name": "Kira and Morrison Music",
+                "url": "https://kiraandmorrisonmusic.com"
             }
         }
     </script>
