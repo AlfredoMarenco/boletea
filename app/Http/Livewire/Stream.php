dde9e69b6e16625914526a90c0f1bc27805f1fd2
unknown <alfredomarenco@boletea.com>
2022-05-17 12:51:44 -0500

    Cmbios

--- app/Http/Livewire/Stream.php
+++ app/Http/Livewire/Stream.php
@@ -7,7 +7,7 @@ use Livewire\Component;
 
 class Stream extends Component
 {
-    public $show=false,$barcode;
+    public $show=false,$barcode,$mensaje='',$code;
 
     public function mount(){
         if (session()->has('show')) {
@@ -16,11 +16,23 @@ class Stream extends Component
     }
 
     public function scan(){
-        $code = Barcode::where('barcode',$this->barcode);
-        if ($code->count()>0) {
-            $this->show = true;
-            session()->put('show', true);
+        $code = Barcode::where('barcode',$this->barcode)->first();
+        $this->code = $code;
+        if ($code) {
+            if ($code->status == 'ACTIVO') {
+                $this->show = true;
+                $code->status = 'ESCANEADO';
+                $code->update();
+                session()->put('show', true);
+            }else{
+                $this->mensaje = 'El código ya ha sido escaneado';
+            }
+
+        }else{
+            $this->mensaje = 'Código no encontrado';
         }
+
+        $this->reset('barcode');
     }
     public function render()
     {
--- resources/views/livewire/stream.blade.php
+++ resources/views/livewire/stream.blade.php
@@ -1,9 +1,18 @@
 <div>
-    <div class="text-center flex justify-center">
-        <x-jet-input class="block w-full" type="text" placeholder="Buscar evento" wire:model="barcode" />
-        <x-jet-button class="mt-2" wire:click="searchBarcode()">Ingresar</x-jet-button>
-    </div>
+    @if ($show == false)
+        <div class="text-center">
+            <img src="{{ asset('img/PORTADA STREAM PADRE ANGEL.jpg') }}" class="w-1/2 mx-auto mb-10" alt="">
+            @if ($mensaje != '')
+                <span>{{ $mensaje }}</span>
+            @endif
+            <x-jet-input class="w-full" type="text" wire:keydown.enter='scan()' placeholder="Ingresar tu codigo"
+                wire:model="barcode" />
+            <x-jet-button class="mt-2 border-red-700" wire:click="scan()">Ingresar</x-jet-button>
+        </div>
+    @endif
     @if ($show == true)
-        hola
+        <video class="mx-auto" id="player" autoplay muted playsinline controls data-poster="/path/to/poster.jpg">
+            <source src="{{ asset('videos/ConferenciaPAdre.mp4') }}" type="video/mp4" />
+        </video>
     @endif
 </div>
