0d7d3d5c5a9c698af82b9ed1940b77c2bf744bcd
unknown <alfredomarenco@boletea.com>
2022-06-10 10:33:57 -0500

    Cambios

--- resources/views/landing/include/navbar.blade.php
+++ resources/views/landing/include/navbar.blade.php
@@ -129,7 +129,7 @@
                                 Politicas de privacidad
                             </span>
                         </a>
-                        <a href="#" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                        <a href="{{ route('cdvs') }}" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
                             <svg class="flex-shrink-0 h-6 w-6 text-rojo" xmlns="http://www.w3.org/2000/svg"
                                 class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
