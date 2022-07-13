6a1d53b9b3f853b4d050e3e2062cce68d2a6b466
unknown <alfredomarenco@boletea.com>
2022-07-13 10:53:44 -0500

    Cambios

--- resources/views/livewire/event-list.blade.php
+++ resources/views/livewire/event-list.blade.php
@@ -38,7 +38,7 @@
                                     {{ $event->fecha }}</p>
                             </div>
                             <div class="col-span-3 pt-4 w-full place-self-end">
-                                <a href="{{ route('showEvent', $event) }}"
+                                <a href="{{ $event->link }}"
                                     class=" text-center block px-3 py-2 bg-rojo text-white font-bold hover:bg-red-600 shadow-lg">Comprar
                                     Boletos</a>
                             </div>
