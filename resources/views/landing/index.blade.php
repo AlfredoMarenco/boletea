0cf7c38272ab8671784ca9144541a42d8b013c37
unknown <alfredomarenco@boletea.com>
2022-08-12 09:03:34 -0500

    Cambios

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -150,12 +150,12 @@
         </div>
     </section>
     <section>
-        <div class="block lg:hidden">
+        <div class="grid md:grid-cols-3 gap-6 items-center h-5 bg-white mb-4 mx-4 md:mx-44 shadow-md lg:hidden">
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5976709800052610"
-                crossorigin="anonymous"></script>
+                    crossorigin="anonymous"></script>
             <!-- Anuncio 1 -->
-            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-5976709800052610"
-                data-ad-slot="1210961234" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-5976709800052610" data-ad-slot="1210961234"
+                data-ad-format="auto" data-full-width-responsive="true"></ins>
             <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
             </script>
