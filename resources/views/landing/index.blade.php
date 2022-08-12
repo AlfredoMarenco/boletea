eb9789fb20f3fa58fe8692c8569a6b5dffc8aca6
unknown <alfredomarenco@boletea.com>
2022-08-12 09:13:08 -0500

    Cambios

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -126,6 +126,16 @@
 @section('content')
     <section class="">
         @livewire('search')
+        <div class="grid md:grid-cols-3 gap-6 items-center h-5 bg-white mb-4 mx-4 md:mx-44 shadow-md lg:hidden">
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5976709800052610"
+                crossorigin="anonymous"></script>
+            <!-- search -->
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-5976709800052610"
+                data-ad-slot="9361422184" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
         @livewire('event-list')
     </section>
 
