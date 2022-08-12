94bee7f8a386900871674a58495aa3dfca97fb47
unknown <alfredomarenco@boletea.com>
2022-08-12 09:14:34 -0500

    Cambios

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -130,8 +130,8 @@
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5976709800052610"
                 crossorigin="anonymous"></script>
             <!-- search -->
-            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-5976709800052610"
-                data-ad-slot="9361422184" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px"
+                data-ad-client="ca-pub-5976709800052610" data-ad-slot="9361422184"></ins>
             <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
             </script>
