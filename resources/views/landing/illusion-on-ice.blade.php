2e2c36de76fc2f1df9cd53cb5ecdfa402e553ce4
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2022-09-20 18:19:12 -0500

    cambios

--- resources/views/landing/illusion-on-ice.blade.php
+++ resources/views/landing/illusion-on-ice.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section class="py-4">
-        <div class="mb-6">
+{{--         <div class="mb-6">
             <div class="grid grid-cols-1 lg:grid-cols-5 place-content-center gap-6 h-36 px-4 lg:px-24 mt-4 mb-6">
                 <div class="hidden lg:block lg:col-span-2 bg-gray-200 shadow-lg">
                     <img src="{{ asset('img/imgeIllusionOnIce.png') }}" class="object-center object-cover " alt="">
@@ -262,7 +262,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
         <div class="mb-6">
             <div class="grid grid-cols-1 lg:grid-cols-5 place-content-center gap-6 h-36 px-4 lg:px-24 mt-4 mb-6">
                 <div class="hidden lg:block lg:col-span-2 bg-gray-200 shadow-lg">
