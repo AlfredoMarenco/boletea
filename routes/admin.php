5dadbf87e8f7b5686573acee2ca516083fefb294
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2022-11-01 17:55:48 -0600

    Colas de mensajes

--- routes/admin.php
+++ routes/admin.php
@@ -3,12 +3,18 @@
 use App\Http\Controllers\Admin\CalendarController;
 use App\Http\Controllers\Admin\RefundController;
 use App\Http\Livewire\Admin\Calendar\ShowEvent;
+use App\Jobs\SendEmails;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/refunds', [RefundController::class, 'index'])->name('refunds.index');
 Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar.index');
 Route::post('/calendar/{recinto?}', [CalendarController::class, 'filter'])->name('calendar.filter');
 Route::get('/calendar/event/{calendar}',ShowEvent::class)->name('calendar.show');
+Route::get('/sendemail',function(){
+    SendEmails::dispatch('alfredomarenco@boletea.com');
+    //Artisan::call('queue:work');
+});
 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
