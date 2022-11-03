9c9c562a52e7e36671916f68fde7f64b6eb35b18
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2022-11-03 10:42:00 -0600

    cambios

--- routes/admin.php
+++ routes/admin.php
@@ -11,13 +11,7 @@ Route::get('/refunds', [RefundController::class, 'index'])->name('refunds.index'
 Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar.index');
 Route::post('/calendar/{recinto?}', [CalendarController::class, 'filter'])->name('calendar.filter');
 Route::get('/calendar/event/{calendar}',ShowEvent::class)->name('calendar.show');
-Route::get('/sendemail',function(){
-    for ($i=0; $i < 5 ; $i++) {
-        SendEmails::dispatch('alfredomarenco@boletea.com');
-    }
-    //Artisan::call('queue:work');
-    return "Email enviado";
-});
+
 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
