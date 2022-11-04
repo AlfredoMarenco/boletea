db4b69add545cf8203c9b47587ee035bff276472
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2022-11-04 17:35:24 -0600

    Cambios

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -14,12 +14,12 @@ class LandingController extends Controller
     {
         $date = date('Ymd');
         $events = Event::where('visible', 'LIKE', 'si')->where('fechaBusqueda', '>=', $date)->orderBy('fechaBusqueda')->get();
-        $clients = Client::where('status',null)->take(10)->get();
+        /* $clients = Client::where('status',null)->take(10)->get();
         foreach ($clients as $client) {
             SendEmails::dispatch($client->email);
             $client->status = "send";
             $client->save();
-        }
+        } */
         return view('landing.index', compact('events'));
     }
 
