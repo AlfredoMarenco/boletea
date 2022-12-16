3523b6d70b414f827d9c9bef1a9f9b8608fb1b33
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2022-12-16 16:31:34 -0600

    Cambios

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('/codereadr',function(Request $request){
-    return $request->all();
+    return $request;
 });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
