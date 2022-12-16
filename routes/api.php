2942802b1a08983d0c11a15ddf2bba01266c417c
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2022-12-16 16:59:12 -0600

    Cambios4

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/codereadr',function($request){
-    return $request;
+Route::post('/codereadr',function(Request $request){
+    return $request['barcode'];
 });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
