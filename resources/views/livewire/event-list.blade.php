03e377ee3e026de495ad620d11973810b5f3dc42
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2023-01-24 11:22:52 -0600

    cambios

--- resources/views/livewire/event-list.blade.php
+++ resources/views/livewire/event-list.blade.php
@@ -48,7 +48,7 @@
                                     <a href="#"
                                         class=" text-center block px-3 py-2 bg-rojo text-white font-bold hover:bg-red-600 shadow-lg">Próximamente</a>
                                         @else
-                                        <a href="{{ $event->link }}"
+                                        <a href="{{ route('showEvent', $event) }}"
                                         class=" text-center block px-3 py-2 bg-rojo text-white font-bold hover:bg-red-600 shadow-lg">Comprar
                                         Boletos</a>
                                     @endif
