21dc2fed11c39fd060b29b0058e866c300a6a599
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-05-18 21:08:05 -0600

    Agregamos condicion de countdown

--- resources/views/livewire/countdown.blade.php
+++ resources/views/livewire/countdown.blade.php
@@ -1,5 +1,5 @@
 <div wire:poll.1000ms='counter'>
-    <div class="text-2xl text-rojo font-bold shadow-md">
+    <div class="text-2xl text-rojo font-bold  mx-auto">
         Inicio de venta en:
         <br>
         {{ $countdown }}
