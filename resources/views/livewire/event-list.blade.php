e32968bfd7332952ec3807ac01aaf407f2e8c5e6
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-08-06 20:35:39 -0600

    correcciones de estilos

--- resources/views/livewire/event-list.blade.php
+++ resources/views/livewire/event-list.blade.php
@@ -40,7 +40,7 @@
 
                     <!-- Botón -->
                     <a href="{{ route('showEvent', $event) }}"
-                        class="mt-4 block w-full text-xs lg:text-md text-center bg-rojo text-white font-semibold py-1 lg:py-2 rounded-md hover:bg-red-600 transition">
+                        class="mt-4 block w-full text-xs lg:text-lg text-center bg-rojo text-white font-semibold py-1 lg:py-2 rounded-md hover:bg-red-600 transition">
                         {{ $event->text_button }}
                     </a>
                 </div>
