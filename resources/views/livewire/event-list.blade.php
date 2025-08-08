123517b95e87dc5b8a4f35a3e7e341a01a090ed3
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-08-07 21:34:38 -0600

    correccion final de los titulos

--- resources/views/livewire/event-list.blade.php
+++ resources/views/livewire/event-list.blade.php
@@ -28,7 +28,7 @@
 
                 <!-- Contenido -->
                 <div class="p-4 space-y-2 flex flex-col flex-1 content-end">
-                    <h3 class="text-sm lg:text-lg font-bold text-gray-800">
+                    <h3 class="text-sm lg:text-md font-bold text-gray-800">
                         {{ $event->title }}
                     </h3>
                     <p class="text-xs lg:text-sm text-gray-600">
