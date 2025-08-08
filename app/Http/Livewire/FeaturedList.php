7285843fb1a996cbf8c91ef287b8106ae5157534
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-08-07 22:45:36 -0600

    Eventos destacados

--- app/Http/Livewire/FeaturedList.php
+++ app/Http/Livewire/FeaturedList.php
@@ -14,7 +14,6 @@ class FeaturedList extends Component
             'events' => Event::where('visible', 'LIKE', 'si')
                 ->where('fechaBusqueda', '>=', $fecha)
                 ->where('featured', 'LIKE', 1)
-                ->orderBy('fechaBusqueda', 'asc')
                 ->inRandomOrder()
                 ->take(4)
                 ->get()
