64f291ec30cc47f476954f6452ecc05d7933f7b4
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-08-07 21:11:42 -0600

    gridcol5 en lista de eventos

--- resources/views/livewire/event-list.blade.php
+++ resources/views/livewire/event-list.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Grid de eventos -->
     <div
-        class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-2 md:gap-2 lg:gap-8 xl:gap-8 px-4 md:px-16 mb-12">
+        class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-2 md:gap-2 lg:gap-8 xl:gap-8 px-4 md:px-16 mb-12">
         @foreach ($events as $event)
             <div wire:key="event-{{ $event->id }}"
                 class="bg-white rounded-2xl overflow-hidden shadow-md border border-gray-200 hover:shadow-xl transition duration-300">
