1a219b2a2509cda066608957c8773d3f33c42538
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-11-27 15:36:36 -0600

    Bolepay

--- resources/views/landing/event.blade.php
+++ resources/views/landing/event.blade.php
@@ -129,7 +129,7 @@
                         <p>No olvides ingresar tu código BOLEPAY para acceder a los pagos diferidos al momento de comprar
                             tus boletos</p>
                     </div>
-                    <img src="{{ asset('LOGOBOLEPAY.png') }}" alt="">
+                    <img src="{{ asset('img/LOGOBOLEPAY.png') }}" alt="">
                 </div>
             @endif
 
