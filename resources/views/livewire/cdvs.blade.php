a267760a5c2def2ed714e2f89a0305ed102da50f
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2025-12-09 14:19:10 -0600

    Cambios

--- resources/views/livewire/cdvs.blade.php
+++ resources/views/livewire/cdvs.blade.php
@@ -313,7 +313,7 @@
             <div class="col-span-4 mt-6">
                 <div class="grid grid-cols-1 lg:grid-cols-4 gap-4 justify-items-center">
                     <div class="text-center bg-white rounded shadow-lg py-4 px-1.5">
-                        <img src="{{ asset('cdvs/logo 2 potrillos.png') }}" class="mx-auto mb-2" alt="">
+                        <img src="{{ asset('cdvs/logo 2 potrillos.png') }}" class="mx-auto mb-2 w-1/4" alt="">
                         <p class="text-sm font-semibold mb-2"><span class="font-bold text-lg text-rojo">Los Dos
                                 Potrillos<br></span>Av. José Maria Morelos 368 Local 2 Centro, 35000 Gómez Palacio, Dgo.</p>
                         <p class="text-xs font-semibold"><span class="font-bold">Horarios: <br></span>Lunes -
