501ff736c665a3728861e87c95502a2dd0051471
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2026-02-05 16:49:51 -0600

    Cambios

--- resources/views/landing/event.blade.php
+++ resources/views/landing/event.blade.php
@@ -59,13 +59,37 @@
 </script>
 @endpush
 
-{{-- @push('publish')
-    <script>
-        (function(s) {
-            s.dataset.zone = '10079145', s.src = 'https://forfrogadiertor.com/tag.min.js'
-        })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
-    </script>
-@endpush --}}
+@if ($event->name == 'ricardomontanertorreon')
+    @push('publish')
+        <!-- Meta Pixel Code -->
+        <script>
+            ! function(f, b, e, v, n, t, s) {
+                if (f.fbq) return;
+                n = f.fbq = function() {
+                    n.callMethod ?
+                        n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+                };
+                if (!f._fbq) f._fbq = n;
+                n.push = n;
+                n.loaded = !0;
+                n.version = '2.0';
+                n.queue = [];
+                t = b.createElement(e);
+                t.async = !0;
+                t.src = v;
+                s = b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t, s)
+            }(window, document, 'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '985725082295453');
+            fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+                src="https://www.facebook.com/tr?id=985725082295453&ev=PageView&noscript=1" /></noscript>
+        <!-- End Meta Pixel Code -->
+    @endpush
+@endif
+
 @section('content')
     @php
         // Variable para la región del usuario (debes pasar esta info desde backend)
--- resources/views/landing/events-torreon.blade.php
+++ resources/views/landing/events-torreon.blade.php
@@ -1,11 +1,4 @@
 @extends('layouts.template')
-{{-- @push('publish')
-    <script>
-        (function(s) {
-            s.dataset.zone = '10079145', s.src = 'https://forfrogadiertor.com/tag.min.js'
-        })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
-    </script>
-@endpush --}}
 
 @section('slider')
     <section>
